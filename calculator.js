43bbda24994740318849cad545453713cb1da51d
Ralph van Schaik <ralph.van.schaik@me.com>
2021-02-24 18:28:02 +0100

    Formpje draait

--- calculator.js
+++ calculator.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send("<h1>Hello Wild World !</h1>")
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, () => {
