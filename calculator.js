5e541d36e649e6f37b1734708a4e3365608e3e38
Ralph van Schaik <ralph.van.schaik@me.com>
2021-02-26 12:22:24 +0100

    bmi calculator klaar

--- calculator.js
+++ calculator.js
@@ -1,9 +1,29 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/bmicalculator', function (req,res) {
+  res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/', function (req, res) {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  res.send('The result is ' + result);
+});
+
+app.post('/bmicalculator', function (req, res){
+  let length = parseFloat(req.body.length);
+  let weight = parseFloat(req.body.weight);
+  let bmi = weight / (length * length);
+  res.send('Your bmi is ' + bmi);
 });
 
 app.listen(port, () => {
